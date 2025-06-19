b879a96ad3bb8a83ce6c7fc16eba8a2f8ecfd753
Alimadcorp <119115454+Alimadcorp@users.noreply.github.com>
2025-06-19 13:04:04 +0500

    FInalized server

---
+++ ChatMadNative/src/main.rs
@@ -0,0 +1,8 @@
+use slint::ComponentHandle;
+
+slint::include_modules!();
+
+fn main() {
+    let main_window = MainWindow::new().unwrap();
+    main_window.run().unwrap();
+}
--- ChatMadServer/src/lib.rs
+++ ChatMadServer/src/lib.rs
@@ -6,8 +6,10 @@ pub struct User {
     identity: Identity,
     #[unique]
     name: String,
+    pfp: String,
     status: Option<String>,
     lastActive: Timestamp,
+    joined: Timestamp,
     aura: i32,
     online: bool,
 }
@@ -18,7 +20,6 @@ pub struct Message {
     channel: String,
     sent: Timestamp,
     text: String,
-    dm: bool,
 }
 
 #[table(name = channel, public)]
@@ -29,6 +30,17 @@ pub struct Channel {
     id: String,
     created: Timestamp,
     description: Option<String>,
+    dm: bool,
+}
+
+#[table(name = stats, public)]
+pub struct Stats {
+    users: i32,
+    messages: i32,
+    channels: i32,
+    dms: i32,
+    totalAura: i32,
+    usersOnline: i32,
 }
 
 #[reducer]
@@ -37,6 +49,20 @@ pub fn set_name(ctx: &ReducerContext, _name: String) -> Result<(), String> {
     if let Some(user) = ctx.db.user().identity().find(ctx.sender) {
         ctx.db.user().identity().update(User {
             name: aname,
+            ..user
+        });
+        Ok(())
+    } else {
+        Err("Cannot set name for unknown user".to_string())
+    }
+}
+
+#[reducer]
+pub fn set_pfp(ctx: &ReducerContext, _url: String) -> Result<(), String> {
+    let url = _url;
+    if let Some(user) = ctx.db.user().identity().find(ctx.sender) {
+        ctx.db.user().identity().update(User {
+            pfp: url,
             ..user
         });
         Ok(())
@@ -58,6 +84,22 @@ pub fn set_status(ctx: &ReducerContext, _status: String) -> Result<(), String> {
     }
 }
 
+
+#[reducer]
+pub fn create_channel(ctx: &ReducerContext, _name: String, desc: String, id: String, isdm: bool) -> Result<(), String> {
+    let chname = validate(_name, 32)?;
+    let _id = id;
+    ctx.db.channel().insert(Channel {
+        creator: ctx.identity(),
+        name: chname,
+        id: _id,
+        created: ctx.timestamp,
+        description: Some(desc),
+        dm: isdm,
+    });
+    Ok(())
+}
+
 #[reducer]
 pub fn send_message(
     ctx: &ReducerContext,
@@ -65,14 +107,13 @@ pub fn send_message(
     _channel: String,
     _dm: bool,
 ) -> Result<(), String> {
-    let text = validate(text, 32)?;
+    let text = validate(text, 1000)?;
     log::info!("{}", text);
     ctx.db.message().insert(Message {
         sender: ctx.sender,
         channel: _channel,
         text,
         sent: ctx.timestamp,
-        dm: _dm,
     });
     if let Some(user) = ctx.db.user().identity().find(ctx.sender) {
         ctx.db.user().identity().update(User {
@@ -102,30 +143,26 @@ pub fn client_connected(ctx: &ReducerContext) {
             online: true,
             ..user
         });
+        //let mut statObj = ctx.db.stats().iter();
     } else {
+        let image1 = "https://i.ibb.co/KjY8zr9R/1.png";
+        let image2 = "https://i.ibb.co/DDhxzxBW/2.png";
+        let image3 = "https://i.ibb.co/wF9j64TF/3.png";
+        let image4 = "https://i.ibb.co/Q7CHKpVh/4.png";
+        let images= vec![image1.to_string(), image2.to_string(), image3.to_string(), image4.to_string()];
+        let to_put: usize = (ctx.timestamp.to_micros_since_unix_epoch() % 4 as i64) as usize;
+        let img = images[to_put].clone();
         ctx.db.user().insert(User {
             name: ctx.timestamp.to_string(),
             status: None,
+            pfp: img,
             lastActive: ctx.timestamp,
+            joined: ctx.timestamp,
             aura: 1,
             identity: ctx.sender,
             online: true,
         });
     }
-}
-
-#[reducer]
-pub fn create_channel(ctx: &ReducerContext, _name: String, desc: String) -> Result<(), String> {
-    let chname = validate(_name, 32)?;
-    let _id = ctx.timestamp.to_micros_since_unix_epoch().to_string();
-    ctx.db.channel().insert(Channel {
-        creator: ctx.identity(),
-        name: chname,
-        id: _id,
-        created: ctx.timestamp,
-        description: Some(desc)
-    });
-    Ok(())
 }
 
 #[reducer(client_disconnected)]
